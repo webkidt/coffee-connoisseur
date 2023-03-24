a8fc12cb36467e4687f9fdccaee7c02a01f7cb28
David Pam <niratpam@gmail.com>
2023-03-24 02:09:57 +0100

    Add Layout throughout all the pages with _app.js e.g Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2023 niratpam</p>
+      </footer>
+    </div>
+  )
 }
