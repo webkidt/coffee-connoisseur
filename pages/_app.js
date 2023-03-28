2ebcfb5a6ed8b9c89b9223363f137462776caf38
David Pam <niratpam@gmail.com>
2023-03-28 06:22:51 +0100

    Start building Coffee Connoisseur next.js app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div>
-      <Component {...pageProps} />
-      <footer>
-        <p>© 2023 niratpam</p>
-      </footer>
-    </div>
-  )
+  return <Component {...pageProps} />
 }
